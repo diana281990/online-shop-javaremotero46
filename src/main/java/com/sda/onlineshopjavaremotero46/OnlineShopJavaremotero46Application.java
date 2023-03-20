2eed865deb1ac82dc4a56e269e03ea10b8f0012f
Diana Vieru <vieruadriana1990@gmail.com>
2023-03-20 20:31:37 +0200

    initial commit

---
+++ src/main/java/com/sda/onlineshopjavaremotero46/OnlineShopJavaremotero46Application.java
@@ -0,0 +1,13 @@
+package com.sda.onlineshopjavaremotero46;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineShopJavaremotero46Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineShopJavaremotero46Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/sda/onlineshopjavaremotero46/OnlineShopJavaremotero46ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sda.onlineshopjavaremotero46;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineShopJavaremotero46ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
