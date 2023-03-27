4d475e883ce8410672b90a7bc837785fb1cd0d48
Diana Vieru <vieruadriana1990@gmail.com>
2023-03-27 21:33:53 +0300

    implemented img field,error,login html

--- src/main/java/com/sda/onlineshopjavaremotero46/controller/MainController.java
+++ src/main/java/com/sda/onlineshopjavaremotero46/controller/MainController.java
@@ -5,19 +5,19 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 import com.sda.onlineshopjavaremotero46.service.ProductService;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @Slf4j
 public class MainController {
     @Autowired
     private ProductService productService;
+
     @GetMapping("/addProduct")
     public String addProductGet(Model model) {
 
@@ -29,26 +29,42 @@ public class MainController {
     }
 
     @PostMapping("/addProduct")
-    public String addProductPost(@ModelAttribute ProductDto productDto) {
+    public String addProductPost(@ModelAttribute ProductDto productDto,@RequestParam("productImg") MultipartFile multipartFile) {
         System.out.println(productDto);
         log.info("Diana was here");
-        productService.create(productDto);
+        productService.create(productDto,multipartFile);
 
         return "redirect:/addProduct";
 
     }
-    @GetMapping("/home")
-    public String homeGet(Model model){
 
-        List<ProductDto> productDtoList=productService.getAllProductDtoList();
+    @GetMapping("/home")
+    public String homeGet(Model model) {
+
+        List<ProductDto> productDtoList = productService.getAllProductDtoList();
         model.addAttribute("productDtoList", productDtoList);
         System.out.println(productDtoList);
 
         return "home";
     }
+
     @GetMapping("/product/{productId}")
-    public String viewProductGet(Model model,@PathVariable(value = "productId") String productId){
-        System.out.println("am dat click pe produsul cu id-ul:" + productId);
+    public String viewProductGet(Model model, @PathVariable(value = "productId") String id) {
+        System.out.println("am dat click pe produsul cu id-ul:" + id);
+        Optional<ProductDto> optionalProductDto = productService.getProductDtoById(id);
+        if (optionalProductDto.isEmpty()) {
+            return "error";
+
+        }
+        model.addAttribute("productDto", optionalProductDto.get());
         return "viewProduct";
+    }
+    @GetMapping("/register")
+    public String registerGet(){
+        return "register";
+    }
+    @GetMapping("/login")
+    public String loginSet(){
+        return "login";
     }
 }
